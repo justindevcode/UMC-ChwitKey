cb9373edf43f06e9c3ec44c1b45b7b6fbc755af6
player-geun <player_geun@naver.com>
2023-07-05 21:47:09 +0900

    chore : 패키지 구조 설정

--- src/main/java/com/example/CherrypickServer/CherrypickServerApplication.java
+++ src/main/java/com/example/CherrypickServer/CherrypickServerApplication.java
@@ -1,4 +1,4 @@
-package com.example.CherrypickServer;
+package com.example.cherrypickserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
