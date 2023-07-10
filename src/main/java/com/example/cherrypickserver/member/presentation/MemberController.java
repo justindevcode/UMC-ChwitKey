f1160fc309f453d81c9df158f1ab12415fd83ca9
player-geun <player_geun@naver.com>
2023-07-10 18:35:13 +0900

    feat : 기본적인 Controller, Service, Repository 구현

---
+++ src/main/java/com/example/cherrypickserver/member/presentation/MemberController.java
@@ -0,0 +1,12 @@
+package com.example.cherrypickserver.member.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/members")
+@RequiredArgsConstructor
+@RestController
+public class MemberController {
+    
+}
