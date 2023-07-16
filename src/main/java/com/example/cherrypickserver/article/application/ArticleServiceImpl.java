9a4ee749e4a05fe7507d235e4cea180216d33869
dangnak2 <80161984+dangnak2@users.noreply.github.com>
2023-07-16 17:44:47 +0900

    #27 feat: ArticleServiceImpl 클래스 @Service 애너테이션 추가

--- src/main/java/com/example/cherrypickserver/article/application/ArticleServiceImpl.java
+++ src/main/java/com/example/cherrypickserver/article/application/ArticleServiceImpl.java
@@ -1,7 +1,9 @@
 package com.example.cherrypickserver.article.application;
 
 import com.example.cherrypickserver.article.dto.Request.CreateArticleReq;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ArticleServiceImpl implements ArticleService{
 
   @Override
