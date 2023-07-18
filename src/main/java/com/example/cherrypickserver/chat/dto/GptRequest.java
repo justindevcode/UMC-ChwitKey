06dd73fed604fe8dc2be589e95df1f07b909f05b
letskuku <yujung9288@naver.com>
2023-07-18 23:53:19 +0900

    #28 feat: Chat GPT DTO, mapper 수정 및 구현

--- src/main/java/com/example/cherrypickserver/chat/dto/GptRequest.java
+++ src/main/java/com/example/cherrypickserver/chat/dto/GptRequest.java
@@ -1,35 +1,13 @@
 package com.example.cherrypickserver.chat.dto;
 
-import com.theokanning.openai.completion.chat.ChatCompletionRequest;
-import com.theokanning.openai.completion.chat.ChatMessage;
 import lombok.Getter;
-import lombok.Setter;
-
-import java.util.List;
 
 @Getter
-@Setter
 public class GptRequest {
 
-    private final String model = "gpt-3.5-turbo";
+    private Long memberId;
 
-    private String role;
+    private Long articleId;
 
-    private String message;
-
-    private final Integer maxTokens = 400;
-
-    //GptRequest to ChatCompletionRequest
-    public static ChatCompletionRequest of(GptRequest gptRequest) {
-        return ChatCompletionRequest.builder()
-                .model(gptRequest.getModel())
-                .messages(convertChatMessage(gptRequest))
-                .maxTokens(gptRequest.getMaxTokens())
-                .build();
-    }
-
-    //GptRequest에서 ChatCompletion의 messages로 들어갈 부분 convert
-    private static List<ChatMessage> convertChatMessage(GptRequest gptRequest) {
-        return List.of(new ChatMessage(gptRequest.getRole(), gptRequest.getMessage()));
-    }
+    private String question;
 }
