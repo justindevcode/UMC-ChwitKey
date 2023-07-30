add80013ebe9ae4d1465b90c135815842734cea6
letskuku <yujung9288@naver.com>
2023-07-31 03:25:56 +0900

    #50 feat: Gpt 설정 시 필요한 기사 분야 값 추가
    현재 설정된 value는 gpt 설정 값으로 그대로 넣기엔 애매한 값들이 있어 한글로 명확화

--- src/main/java/com/example/cherrypickserver/article/domain/Industry.java
+++ src/main/java/com/example/cherrypickserver/article/domain/Industry.java
@@ -3,31 +3,38 @@ package com.example.cherrypickserver.article.domain;
 import com.example.cherrypickserver.article.exception.IndustryNotFoundException;
 
 public enum Industry {
-	STEEL("steel"),
-	PETROLEUM_CHEMICAL("Petroleum/Chemical"),
-	OIL_REFINING("oilrefining"),
-	SECONDARY_BATTERY("secondarybattery"),
-	SEMICONDUCTOR("Semiconductor"),
-	DISPLAY("Display"),
-	MOBILE("Mobile"),
-	IT("It"),
-	CAR("car"),
-	SHIPBUILDING("Shipbuilding"),
-	SHIPPING("Shipping"),
-	FNB("FnB"),
-	RETAIL_DISTRIBUTION("RetailDistribution"),
-	CONSTRUCTION("Construction"),
-	HOTEL_TRAVEL("HotelTravel"),
-	FIBER_CLOTHING("FiberClothing");
+	STEEL("steel", "철강"),
+	PETROLEUM_CHEMICAL("Petroleum/Chemical", "석유&화학"),
+	OIL_REFINING("oilrefining", "정유"),
+	SECONDARY_BATTERY("secondarybattery", "2차 전지"),
+	SEMICONDUCTOR("Semiconductor", "반도체"),
+	DISPLAY("Display", "디스플레이"),
+	MOBILE("Mobile", "휴대폰"),
+	IT("It", "IT"),
+	CAR("car", "자동차"),
+	SHIPBUILDING("Shipbuilding", "조선"),
+	SHIPPING("Shipping", "해운"),
+	FNB("FnB", "F&B"),
+	RETAIL_DISTRIBUTION("RetailDistribution", "소매유통"),
+	CONSTRUCTION("Construction", "건설"),
+	HOTEL_TRAVEL("HotelTravel", "호텔&여행&항공"),
+	FIBER_CLOTHING("FiberClothing", "섬유&의류");
 
 	private final String value;
 
-	Industry(String value) {
+	private final String valueGpt;
+
+	Industry(String value, String valueGpt) {
 		this.value = value;
+		this.valueGpt = valueGpt;
 	}
 
 	public String getValue() {
 		return value;
+	}
+
+	public String getValueGpt() {
+		return valueGpt;
 	}
 
 	public static Industry fromValue(String value) {
