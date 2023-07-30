bfe71944ee731b18b07fab1a55b02e1d4af943ce
letskuku <yujung9288@naver.com>
2023-07-31 01:58:20 +0900

    #50 feat: 선택 기능 예외처리 구현

---
+++ src/main/java/com/example/cherrypickserver/chat/presentation/ChatExceptionController.java
@@ -0,0 +1,25 @@
+package com.example.cherrypickserver.chat.presentation;
+
+import com.example.cherrypickserver.chat.exception.ChatSelectNotFoundException;
+import com.example.cherrypickserver.chat.exception.SelectTypeNotFoundException;
+import com.example.cherrypickserver.global.dto.ResponseCustom;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class ChatExceptionController {
+
+    @ExceptionHandler(ChatSelectNotFoundException.class)
+    public ResponseCustom<String> catchChatSelectNotFoundException(ChatSelectNotFoundException e) {
+        log.error(e.getMessage());
+        return ResponseCustom.NOT_FOUND(e.getMessage());
+    }
+
+    @ExceptionHandler(SelectTypeNotFoundException.class)
+    public ResponseCustom<String> catchSelectTypeNotFoundException(SelectTypeNotFoundException e) {
+        log.error(e.getMessage());
+        return ResponseCustom.NOT_FOUND(e.getMessage());
+    }
+}
