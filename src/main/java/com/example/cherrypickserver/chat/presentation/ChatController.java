a4d9257791a9dffdd1a7dbc6e5f88d2e30c7b15e
letskuku <yujung9288@naver.com>
2023-08-02 02:04:27 +0900

    #58 feat: chat controller 파라미터 설명 추가

--- src/main/java/com/example/cherrypickserver/chat/presentation/ChatController.java
+++ src/main/java/com/example/cherrypickserver/chat/presentation/ChatController.java
@@ -7,6 +7,8 @@ import com.example.cherrypickserver.chat.dto.request.QuestionRequest;
 import com.example.cherrypickserver.chat.dto.response.ChatResponse;
 import com.example.cherrypickserver.global.dto.ResponseCustom;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -42,11 +44,17 @@ public class ChatController {
         return ResponseCustom.OK(chatService.chatQuestion(questionRequest));
     }
 
-    @Operation(summary = "선택 기능", description = "버튼으로 제시된 세 기능 중 하나를 요청합니다.")
+    @Operation(summary = "선택 기능",
+            description = "버튼으로 제시된 세 기능 중 하나를 요청합니다. "
+                    + "기능은 type(summary, translation, keyword)에 따라 요약, 영어 번역, 중요 키워드 추출로 구분됩니다.")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "답변 생성 성공"),
             @ApiResponse(responseCode = "404", description = "존재하지 않는 기사"),
             @ApiResponse(responseCode = "404", description = "존재하지 않는 기능")
+    })
+    @Parameters({
+            @Parameter(name = "articleId", description = "기사 아이디"),
+            @Parameter(name = "type", description = "summary(요약), translation(번역), keyword(키워드 추출)")
     })
     @GetMapping("/select/{articleId}")
     public ResponseCustom<GptResponse> chatSelect(@PathVariable Long articleId, @RequestParam String type) {
