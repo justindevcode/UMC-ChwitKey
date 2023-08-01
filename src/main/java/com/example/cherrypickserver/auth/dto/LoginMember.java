76bad41390cdbe9d584492206a487e559846bc13
player-geun <player_geun@naver.com>
2023-08-01 12:31:58 +0900

    feat : OAuth 사용자를 입력받는 클래스 및 인증 객체 커스텀 구현

---
+++ src/main/java/com/example/cherrypickserver/auth/dto/LoginMember.java
@@ -0,0 +1,14 @@
+package com.example.cherrypickserver.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class LoginMember {
+
+    private Long id;
+    private String name;
+}
