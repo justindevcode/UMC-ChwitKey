76a12e941c593a5a736c5d6f862b4a13b8b59280
player-geun <player_geun@naver.com>
2023-08-01 12:30:06 +0900

    feat : security config 구현

---
+++ src/main/java/com/example/cherrypickserver/global/config/SecurityConfig.java
@@ -0,0 +1,71 @@
+package com.example.cherrypickserver.global.config;
+
+import com.example.cherrypickserver.auth.application.CustomOAuth2UserService;
+import com.example.cherrypickserver.auth.application.JwtProvider;
+import com.example.cherrypickserver.auth.presentation.JwtAuthenticationFilter;
+import com.example.cherrypickserver.auth.presentation.OAuth2LoginFailureHandler;
+import com.example.cherrypickserver.auth.presentation.OAuth2LoginSuccessHandler;
+import com.example.cherrypickserver.member.application.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
+    private final OAuth2LoginFailureHandler oAuth2LoginFailureHandler;
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final JwtProvider jwtProvider;
+    private final MemberService memberService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
+
+        http
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http
+                .authorizeHttpRequests(request -> {request
+                        .antMatchers("/", "/auth/**", "/oauth2/**").permitAll()
+                        .anyRequest().authenticated();
+                });
+
+        http
+                .oauth2Login(oauth2 -> {oauth2
+                        .successHandler(oAuth2LoginSuccessHandler)
+                        .failureHandler(oAuth2LoginFailureHandler)
+                        .redirectionEndpoint(endpoint -> endpoint.baseUri("/oauth2/callback/*"))
+                        .authorizationEndpoint(endpoint -> endpoint.baseUri("/auth/authorize"))
+                        .userInfoEndpoint(endpoint -> endpoint.userService(customOAuth2UserService));
+                });
+
+        http
+                .addFilterAfter(jwtAuthenticationProcessingFilter(), LogoutFilter.class);
+
+        http
+                .cors(httpSecurityCorsConfigurer -> {});
+
+        return http.build();
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationProcessingFilter() {
+        return new JwtAuthenticationFilter(jwtProvider, memberService);
+    }
+}
