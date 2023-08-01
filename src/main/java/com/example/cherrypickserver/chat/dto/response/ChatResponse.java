b88f3435e348348a1356ab13bdca827536ea9906
letskuku <yujung9288@naver.com>
2023-08-02 02:03:38 +0900

    #58 feat: ChatResponse swagger 인삿말 예시 추가

--- src/main/java/com/example/cherrypickserver/chat/dto/response/ChatResponse.java
+++ src/main/java/com/example/cherrypickserver/chat/dto/response/ChatResponse.java
@@ -12,6 +12,8 @@ public class ChatResponse {
     @Schema(description = "채팅방 아이디")
     private Long chatId;
 
-    @Schema(description = "채팅방 시작 인삿말")
+    @Schema(description = "채팅방 시작 인삿말",
+            example = "안녕하세요. 취트키GPT입니다.\n'기사 제목' 기사를 보고 오셨네요.\n"
+            + "기사 내용 중 궁금한 부분이 있다면 물어보세요.")
     private String greeting;
 }
