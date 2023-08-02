6848149145f2ccd384b479a68ec1e1d01c4d20e0
player-geun <player_geun@naver.com>
2023-08-02 23:12:16 +0900

    feat : Swagger 허용 URL 변경 구현

--- src/main/java/com/example/cherrypickserver/global/config/SecurityConfig.java
+++ src/main/java/com/example/cherrypickserver/global/config/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig {
 
         http
                 .authorizeHttpRequests(request -> {request
-                        .antMatchers("/", "/auth/**", "/oauth2/**", "https://umcserver.shop/swagger-ui/index.html").permitAll()
+                        .antMatchers("/", "/auth/**", "/oauth2/**", "https://umcserver.shop/**").permitAll()
                         .anyRequest().authenticated();
                 });
 
