448b9711c890b61d77350d85f74527eda023f869
dangnak2 <80161984+dangnak2@users.noreply.github.com>
2023-08-16 15:20:19 +0900

    #27 feat: 기사 검색 res 기사id, 산업 필드 추가

--- src/main/java/com/example/cherrypickserver/article/dto/response/SearchArticleRes.java
+++ src/main/java/com/example/cherrypickserver/article/dto/response/SearchArticleRes.java
@@ -12,20 +12,24 @@ import java.util.stream.Collectors;
 @Data
 @Builder
 public class SearchArticleRes {
+  private Long articleId;
   private String title;
   private String content;
   private String publisher;
   private String reporter;
   private Date uploadedAt;
+  private String industry;
   private List<SearchArticlePhotoRes> articlePhoto;
 
   public static SearchArticleRes toDto(Article article) {
     return SearchArticleRes.builder()
+            .articleId(article.getId())
             .title(article.getTitle())
             .content(article.getContents())
             .publisher(article.getPublisher())
             .reporter(article.getReporter())
             .uploadedAt(article.getUploadedAt())
+            .industry(article.getIndustry().getValueGpt())
             .articlePhoto(article.getArticlePhoto().stream().map(SearchArticlePhotoRes::toDto).collect(Collectors.toList()))
             .build();
   }
