fe786633face1aa997bd91bee7902a365f8afd26
dangnak2 <80161984+dangnak2@users.noreply.github.com>
2023-09-05 03:17:55 +0900

    #122 feat: spring actuator 설정

--- src/main/java/com/example/cherrypickserver/CherrypickServerApplication.java
+++ src/main/java/com/example/cherrypickserver/CherrypickServerApplication.java
@@ -1,9 +1,11 @@
 package com.example.cherrypickserver;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+@AutoConfiguration
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class CherrypickServerApplication {
 
